8a0147238d6fecb2f083ef70e7b83ab41a2641f1
stepan <stvbuz@gmail.com>
2023-02-08 02:00:55 +0500

    Interface Repository Impl

--- app/src/main/java/com/independed/remark/presentation/MainActivity.kt
+++ app/src/main/java/com/independed/remark/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.independed.remark
+package com.independed.remark.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.independed.remark.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
